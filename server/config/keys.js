77e47c73ad6cb8a7ffbf0a56f084ec58795ef6c3
Abhinay <abhinaybathina2@gmail.com>
2021-04-23 19:59:09 +0530

    TaskMate

---
+++ server/config/keys.js
@@ -0,0 +1,7 @@
+// if (process.env.NODE_ENV === 'production') {
+  
+// } else {
+//   module.exports = require('./keys_dev');
+// }
+
+module.exports = require('./keys_prod');
---
+++ server/config/keys_prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI: "mongodb+srv://EAD:EAD@cluster0.khjb2.mongodb.net/TaskTracker?retryWrites=true&w=majority",
+  secretOrKey: "abhinay",
+  cloudName: process.env.CLOUD_NAME,
+  apiKey: process.env.API_KEY,
+  apiSecret: process.env.API_SECRET,
+};
